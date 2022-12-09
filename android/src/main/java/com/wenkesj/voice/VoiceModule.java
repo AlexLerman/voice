32911487fe76ec56ea17b484d4b3db0200d07aa3
Alex Lerman <alexander.a.lerman@gmail.com>
2022-12-09 16:59:01 -0600

    adding escape for bugging google speech output

--- android/src/main/java/com/wenkesj/voice/VoiceModule.java
+++ android/src/main/java/com/wenkesj/voice/VoiceModule.java
@@ -322,8 +322,11 @@ public class VoiceModule extends ReactContextBaseJavaModule implements Recogniti
     WritableArray arr = Arguments.createArray();
 
     ArrayList<String> matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-    for (String result : matches) {
-      arr.pushString(result);
+    
+    if (matches != null) {
+      for (String result : matches) {
+        arr.pushString(result);
+      }
     }
 
     WritableMap event = Arguments.createMap();
@@ -345,8 +348,10 @@ public class VoiceModule extends ReactContextBaseJavaModule implements Recogniti
     WritableArray arr = Arguments.createArray();
 
     ArrayList<String> matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-    for (String result : matches) {
-      arr.pushString(result);
+    if (matches != null) {
+      for (String result : matches) {
+        arr.pushString(result);
+      }
     }
 
     WritableMap event = Arguments.createMap();
